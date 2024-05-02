04b18853d43dc2658027ed560f08d7c0823a5595
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-05-02 21:55:36 +0200

    update using an API created in the same repository of react. It's possible :)

--- src/components/cards/CardArticles.jsx
+++ src/components/cards/CardArticles.jsx
@@ -1,35 +1,37 @@
 import React from "react";
 import "./cards.css";
 import { useEffect, useState } from "react";
-import axios, { Axios } from "axios";
+import axios from "axios";
 
 const CardArticles = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost/progetto-finalle/server/articles.php").then(function (res) {
-      console.log(res);
+      setPosts(res.data);
     });
   }, []);
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-12 mt-4">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
+      {posts.map(function (val) {
+        return (
+          <div className="row">
+            <div className="col-12 mt-4">
+              <div className="card">
+                <img src={val.imagem} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{val.title}</h5>
+                  <p className="card-text">{val.conteudo}</p>
+                  <a href="#" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
